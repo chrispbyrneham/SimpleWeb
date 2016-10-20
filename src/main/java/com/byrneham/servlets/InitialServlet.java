bdc18fdd05329d8dd9720f09f7842da0f27c14be
Chris Byrneham <chrisby@motabilityoperations.co.uk>
2016-10-20 09:54:14 +0100

    Initial commit

---
+++ src/main/java/com/byrneham/servlets/InitialServlet.java
@@ -0,0 +1,22 @@
+package com.byrneham.servlets;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class InitialServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder buf = new StringBuilder();
+        buf.append("<html>");
+        buf.append("Request from host: ").append(req.getHeader("host")).append("<br/>");
+        buf.append("Request context path: ").append(req.getContextPath());
+
+        buf.append("</html>");
+        resp.getWriter().print(buf.toString());
+    }
+
+}
